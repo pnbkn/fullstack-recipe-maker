d10c83d98a026dacc21fd29a4b871fb4e2d53880
pnbkn <pnblackburn@gmail.com>
2019-10-14 15:38:31 -0700

    Added User detail page based on ID

--- src/User.js
+++ src/User.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const User = ({ users }) => {
+const User = ({ users, match }) => {
+  console.log("ID ", match.params.id)
+  console.log("USERS ", users)
   return (
-    <h1>User</h1>
+    <div>
+      <h1>User</h1>
+      <ul>
+        {
+          users.map(user => user.id === match.params.id ? <li key={user.id}>{user.username}</li> : " ")
+
+        }
+      </ul>
+    </div>
   )
 }
 
-const mapStateToProps = state => ({ users: state.users });
+const mapStateToProps = state => ({ users: state.users, recipes: state.recipes });
 export default connect(mapStateToProps)(User);
